a55c6f94b5dc41869079f1dc5a60b2cdd0873c77
Hut, Michel <michel.a.hut+GCO2@capgemini.com>
2026-04-16 14:56:50 +0200

    test tool 5

---
+++ src/MyTestProject/newestClass.cs
@@ -0,0 +1,11 @@
+namespace MyTestProject;
+
+public class newestClass
+{
+    //this is another tset class
+
+    public void hello()
+    {
+        Console.WriteLine("Hello from newestClass!");
+    }
+}
