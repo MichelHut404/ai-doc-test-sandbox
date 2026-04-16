356772dbef4ab02f9d6b7b98131e7ec99975f50b
Hut, Michel <michel.a.hut+GCO2@capgemini.com>
2026-04-16 15:09:34 +0200

    tool test 6

--- src/MyTestProject/Class2.cs
+++ src/MyTestProject/Class2.cs
@@ -13,4 +13,9 @@ public class Class2
         Console.WriteLine("This is another implementation of the interface method in a different class.");
     }
 
+    public void methodWithParameters(string param1, int param2)
+    {
+        Console.WriteLine($"This method takes parameters: {param1} and {param2}");
+    }
+
 }
